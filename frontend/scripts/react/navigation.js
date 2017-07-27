6e075436319f1d5de981820648469949aecabc6f
erwanledoux <lawanledoux@gmail.com>
2017-07-27 16:22:16 +0200

    added history and homePage style

--- frontend/scripts/react/navigation.js
+++ frontend/scripts/react/navigation.js
@@ -5,11 +5,15 @@ import { Redirect } from 'react-router'
 import App from './containers/App'
 import HomePage from './pages/HomePage'
 import { PageComponentsByComponentsName } from './views'
+import { IS_DEV } from '../utils/config'
 import links from '../utils/links'
 
 export function render (router) {
   // unpack
-  const { location: { pathname, search }, match: { params } } = router
+  const { history,
+    location: { pathname, search },
+    match: { params }
+  } = router
   const { pageName } = params
   // special content case which is not a page
   const componentName = pageName && `${startcase(pageName)}Page`
@@ -19,13 +23,18 @@ export function render (router) {
     return (
       <App
         {...params}
+        history={history}
         pathname={pathname}
       >
       <PageComponent
+        history={history}
         {...params}
       />
     </App>
   )} else {
+    if (IS_DEV) {
+      console.warn(`Did not find a page like ${componentName} in PageComponentsByComponentsName`)
+    }
     // maybe the user typed something for which the first param
     // in the path does not match a known path, so in that
     // case let's return to the home page
