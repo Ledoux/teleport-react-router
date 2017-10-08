416331ad5bb4fbc0540ed9d19f343bc47ef7be5e
erwanledoux <lawanledoux@gmail.com>
2017-10-08 02:23:45 +0200

    removed redux things in root

--- frontend/scripts/utils/root.js
+++ frontend/scripts/utils/root.js
@@ -1,32 +1,18 @@
-mport { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 
 import config from './config'
-import createStore from './store'
-import createRootReducer from '../reducers'
 
 export function createRoot (history) {
   const setup = Object.assign({}, config,
     typeof window !== 'undefined' && window.__SETUP__)
 
-  const initialState = typeof window !== 'undefined' && window.__INITIAL_STATE__
-
-  const rootReducer = createRootReducer({ initialState,
-    history,
-    setup
-  })
-
   const store = createStore({ history,
-    initialState,
-    rootReducer,
     setup
   })
 
   return { history,
-    ProviderComponent: Provider,
     RouterComponent: ConnectedRouter,
-    setup,
-    store
+    setup
   }
 }
 
